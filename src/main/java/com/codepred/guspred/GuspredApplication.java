2285df54184a6e965de664b509cee052622ba034
Jacek <jacek.kaczmarek@cornelsen.de>
2025-03-29 23:07:53 +0100

    init repo

---
+++ src/main/java/com/codepred/guspred/GuspredApplication.java
@@ -0,0 +1,13 @@
+package com.codepred.guspred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuspredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuspredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codepred/guspred/GuspredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepred.guspred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuspredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
