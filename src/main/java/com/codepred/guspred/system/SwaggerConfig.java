0cb9de38b99fce73fc8a22713e44c20cb79c911a
Jacek <jacek.kaczmarek@cornelsen.de>
2025-03-30 13:25:02 +0200

    feat: add SwaggerConfig

---
+++ src/main/java/com/codepred/guspred/system/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.codepred.guspred.system;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://api.guspred.com"))
+                .addServersItem(new Server().url("/"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
